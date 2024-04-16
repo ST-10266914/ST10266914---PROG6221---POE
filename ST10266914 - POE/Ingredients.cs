6acaf621f33e6508513f77b7fb9426fd0b11805a
Keyur Ramdeen Singh <ST10266914@vcconnect.edu.za>
2024-04-16 14:01:24 +0200

    Re-adding Constructor

--- ST10266914 - POE/Ingredients.cs
+++ ST10266914 - POE/Ingredients.cs
@@ -15,8 +15,15 @@ namespace ST10266914___POE
         //A line of code which allows for data to be stored and retrieved in a variable when called//
         public string Measurement {  get; set; }
 
-        public int OriginalQuantity { get; set; }
+        public int OriginalQuantity {  get; set; }
 
-
+        //A constructor for the above variables//
+        public Ingredients(string name, int quantity, string measurement, int originalQuantity)
+        {
+            this.Name = name;
+            this.Quantity = quantity;
+            this.Measurement = measurement;
+            OriginalQuantity = originalQuantity;
+        }
     }
 }
