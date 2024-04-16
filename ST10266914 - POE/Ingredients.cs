22a1989630440313b36a1d78bd739a8d78db3e56
Keyur Ramdeen Singh <ST10266914@vcconnect.edu.za>
2024-04-16 13:55:36 +0200

    Removed unecessary constructor for my get and set variables

--- ST10266914 - POE/Ingredients.cs
+++ ST10266914 - POE/Ingredients.cs
@@ -15,12 +15,8 @@ namespace ST10266914___POE
         //A line of code which allows for data to be stored and retrieved in a variable when called//
         public string Measurement {  get; set; }
 
-        //A constructor for the above variables//
-        public Ingredients(string name, int quantity, string measurement)
-        {
-            this.Name = name;
-            this.Quantity = quantity;
-            this.Measurement = measurement;
-        }
+
+
+
     }
 }
